bd6777dc70eb0a0ba88cf7db35f5273875fe92b9
Caleb-Sho <calebshokunbi619@gmail.com>
2026-01-18 13:40:54 +0100

    Update AdminLayout.jsx

--- src/pages/Admin/AdminLayout.jsx
+++ src/pages/Admin/AdminLayout.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import Sidebar from '../../components/admin/Sidebar';
 import Header from '../../components/admin/Header';
-import Dashboard from './Dashboard';
 import { Outlet } from 'react-router-dom';
 
 export default function AdminLayout() {
